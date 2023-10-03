af2ed745eb635afc69c8223f41ea4d697c5133d0
AleksandrBreslavets <sasha.bresl2612005@gmail.com>
2023-10-03 20:27:30 +0300

    add try-catch block in main.cpp

--- main.cpp
+++ main.cpp
@@ -4,20 +4,27 @@
 #include "readFileData.cpp"
 #include "writeResultToFile.cpp"
 
-int main()  
+int main()
 {
-    FileData<double> fileData = readFileData<double>("data.txt");
-
-    int n = fileData.n;
-    std::pair initialPoint = fileData.initialPoint;
-    std::vector<std::pair<double, double>> baseCoords = fileData.baseCoords;
-
-    FuncObj<double> obj(baseCoords, n, initialPoint);
-
-    for (const auto &coord : obj())
+    try
     {
-        std::cout << coord.first << " " << coord.second << std::endl;
-        writeResultToFile("result.txt", std::to_string(coord.first) + " " + std::to_string(coord.second));
+        FileData<double> fileData = readFileData<double>("data.txt");
+
+        int n = fileData.n;
+        std::pair initialPoint = fileData.initialPoint;
+        std::vector<std::pair<double, double>> baseCoords = fileData.baseCoords;
+
+        FuncObj<double> obj(baseCoords, n, initialPoint);
+
+        for (const auto &coord : obj())
+        {
+            std::cout << coord.first << " " << coord.second << std::endl;
+            writeResultToFile("result.txt", std::to_string(coord.first) + " " + std::to_string(coord.second));
+        }
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
     }
 
     return 0;
